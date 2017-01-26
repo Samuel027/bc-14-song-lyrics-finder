32088225fd1e83011be447400cd5050d8eaf4a2c
Samuel027 <samuelgatuma@gmail.com>
2017-01-26 15:28:56 +0300

    Added the file with the database configuration

---
+++ database.py
@@ -0,0 +1,42 @@
+"""
+
+Filename    :
+Date        :
+Author      :
+Description :
+
+"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class Music(Base):
+        __tablename__ = 'songs'
+        song_id = Column(String, primary_key=True)
+        song_name = Column(String(50))
+        song_artist_name = Column(String(50))
+        song_lyrics = Column(Text())
+
+engine = create_engine('sqlite:///sqlite.db')
+Base.metadata.create_all(engine)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
